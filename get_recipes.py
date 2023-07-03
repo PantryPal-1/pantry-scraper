74457b11ccab2f7ca75ee55df494a034880c7a1a
sayak datta <41303746+sayakdatta@users.noreply.github.com>
2023-07-03 13:42:13 -0400

    added filtering

--- get_recipes.py
+++ get_recipes.py
@@ -5,6 +5,7 @@ import requests
 import time 
 
 # Define the url in python 
+baseUrl = "https://www.jamieoliver.com"
 url = "https://www.jamieoliver.com/recipes/category/course/mains/"
 
 # Fetching html from the website
@@ -12,12 +13,22 @@ page = requests.get(url)
 # BeautifulSoup enables to find the elements/tags in a webpage 
 soup = BeautifulSoup(page.text, "html.parser")
 
-print(soup)
 
-# GET URLS to all recipes available on main
 
-#parse each URL, use recipe-scraper library to get ingredients
+# GET URLS to all recipes available on main HTML Page
+links = []
+for link in soup.find_all('a'):
+	path = link.get('href')
+	if path[0:9] == '/recipes/':
+		links.append(path)
+print(links)
+#
+# Parse through each link and make sure its a recipe link
+
 
 #store in CSV data set
+
+
+
 
 
